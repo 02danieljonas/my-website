5f567f65b4abcccae6c59ec0cda94a878063297c
styled <smyvensestime@gmail.com>
2022-04-13 12:03:11 -0400

    all changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,63 @@
 import React from "react";
-import { useEffect } from "react";
-import useState from "react-usestateref";
+import { useEffect, useState } from "react";
 
 const Navbar = ({ navLinks }) => {
-    const [, setActiveLink, activeLinkRef] = useState(navLinks[0].dataset.name);
+    const [activeLink, setActiveLink] = useState(navLinks[0].dataset.name);
+    const [hide, setHide] = useState(false);
+    const [scrollPos, setScrollPos] = useState(0);
+    const [byMouse, setByMouse] = useState(false); // wheter the navbar is being shown because the user put the mouse at the top of the screen
 
     useEffect(() => {
-        const elementInViewport = el =>
+        const elementInViewport = (el) =>
             el.offsetTop < window.pageYOffset + window.innerHeight &&
             el.offsetTop + el.offsetHeight > window.pageYOffset;
 
-        const scrollListener = window.addEventListener("scroll", () =>
+        const onScroll = () => {
+            // highlight the link that matches with the user scroll position
             navLinks.forEach(
                 element => elementInViewport(element) && setActiveLink(element.dataset.name)
-            )
-        );
+            );
 
-        return () => window.removeEventListener("scroll", scrollListener);
-    }, []);
+            // hide the navbar when scrolling down
+            setHide(scrollPos < window.scrollY && window.scrollY > 100);
+            setScrollPos(window.scrollY);
+        };
+
+        // show navbar when user put the mouse all the way at the top
+        const onMouseMoving = (event) => {
+            if (window.scrollY > 100) {
+                if (event.clientY < 50) {
+                    setHide(false);
+                    setByMouse(true);
+                } else if (byMouse && event.clientY > 50) {
+                    setHide(true);
+                    setByMouse(false);
+                }
+            }
+        };
+
+        window.addEventListener("scroll", onScroll);
+        window.addEventListener("mousemove", onMouseMoving);
+
+        return () => {
+            window.removeEventListener("scroll", onScroll);
+            window.removeEventListener("mousemove", onMouseMoving);
+        };
+    });
 
     return (
-        <nav className="nav">
-            {navLinks.map(element => (
+        <nav
+            className={`nav ${hide && "hide"}`}
+        >
+            {navLinks.map((element, index) => (
                 <div
                     key={Math.floor(Math.random() * 1000000)}
-                    onClick={() => element.scrollIntoView()}
-                    data-active={activeLinkRef.current === element.dataset.name && "true"}
                     className="navlink"
+                    onClick={() =>
+                        // if user click home, then go to the top of the page else go to the element
+                        index === 0 ? window.scrollTo(0, 0) : element.scrollIntoView()
+                    }
+                    data-active={activeLink === element.dataset.name && "true"}
                 >
                     <h3>{element.dataset.name}</h3>
                 </div>
